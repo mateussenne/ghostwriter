efbcb44578f065c48f8f6ba6cbc49123c8c4cded
Mateus Senne <mateussenne@gmail.com>
2023-12-15 14:07:13 -0300

    Fix pre-push.js path on setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ PRE_PUSH_HOOK="../../.git/hooks/pre-push"
 
 if [ -f "$PRE_PUSH_HOOK" ]; then
     # Append custom logic to existing pre-push hook
-    echo -e "\nnode ../../.git/hooks/pre-push.js\nexit 0" > "$PRE_PUSH_HOOK"
+    echo -e "\nnode .git/hooks/pre-push.js\nexit 0" > "$PRE_PUSH_HOOK"
 else
     # Create pre-push hook and add custom logic
     touch ../../.git/hooks/pre-push
-    echo -e "#!/bin/bash\n\nnode ../../.git/hooks/pre-push.js\nexit 0" > "$PRE_PUSH_HOOK"
+    echo -e "#!/bin/bash\n\nnode .git/hooks/pre-push.js\nexit 0" > "$PRE_PUSH_HOOK"
     chmod +x "$PRE_PUSH_HOOK"
 fi
