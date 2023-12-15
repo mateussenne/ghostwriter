e7f0b5a8ca13f5735f61eb2fc4a2e753e723747c
Mateus Senne <mateussenne@gmail.com>
2023-12-15 13:51:54 -0300

    Add code to fetch ghost-writer app locally

--- hooks/pre-push.js
+++ hooks/pre-push.js
@@ -1,43 +1,45 @@
+#!/usr/bin/env node
+
 const { execSync } = require("child_process");
-const { URI } = require("uri-js");
+const URI = require("uri-js");
 
 // Read template content from file
-const template = execSync("cat template.md", { encoding: "utf-8" });
+console.log("starting");
+try {
+  const template = execSync("cat template.md", { encoding: "utf-8" });
+  if (!template) {
+    return console.log("No ghostwriter template found");
+  }
 
-// Get current git branch
+  // Get current git branch
+  const currentBranch = execSync("git rev-parse --abbrev-ref HEAD");
 
-// Get the git diff
-const diffs = execSync("git diff..git rev-parse --abbrev-ref HEAD", {
-  encoding: "utf-8",
-});
+  // Get the git diff
+  const diffs = execSync(`git diff main..${currentBranch}`, {
+    encoding: "utf-8",
+  });
+
+  // Escape template and diffs
+  const escapedTemplate = escapeString(template);
+  const escapedDiffs = escapeString(diffs);
+
+  // Create JSON data without newlines
+  const data = JSON.stringify({
+    template: escapedTemplate,
+    diffs: escapedDiffs,
+  });
+
+  const url = "http://localhost:3000/write";
+
+  const result = fetch(url, { method: "POST", body: data });
+
+  return console.log(result);
+} catch (error) {
+  console.log(error);
+}
 
 // Function to escape special characters in a string
 function escapeString(str) {
+  console.log(str);
   return URI.serialize(URI.parse(str));
 }
-
-// Escape template and diffs
-const escapedTemplate = escapeString(template);
-const escapedDiffs = escapeString(diffs);
-
-// Create JSON data without newlines
-const data = JSON.stringify({
-  template: escapedTemplate,
-  diffs: escapedDiffs,
-});
-
-console.log(data);
-
-// Replace the URL with your actual endpoint
-// const url = "http://localhost:3000/write";
-// const result = execSync(
-//   `curl -X POST -H "Content-Type: application/json" -d '${data}' ${url}`,
-//   { encoding: "utf-8" }
-// );
-// const result = fetch(url, {
-//   body: {
-//     data,
-//   },
-// });
-
-// console.log(result);
